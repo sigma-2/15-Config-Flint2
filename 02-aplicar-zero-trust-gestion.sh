92a51b45e3247f91e0d38cb8067f4f97016f9fc2
javier <javier.vazquez@ua.es>
2025-10-23 23:19:15 +0200

    corregido bug por puertos

--- 02-aplicar-zero-trust-gestion.sh
+++ 02-aplicar-zero-trust-gestion.sh
@@ -4,8 +4,8 @@
 set -e
 echo "[5/5] Zero-trust en servicios de gestión..."
 if [ -f /etc/init.d/uhttpd ]; then
-  uci set uhttpd.main.listen_http='192.168.60.1:80'
-  uci set uhttpd.main.listen_https='192.168.60.1:443'
+  uci set uhttpd.main.listen_http='192.168.60.1:8080'
+  uci set uhttpd.main.listen_https='192.168.60.1:8443'
   uci commit uhttpd
   /etc/init.d/uhttpd restart || true
 fi
@@ -17,6 +17,17 @@ if [ -f /etc/init.d/dropbear ]; then
 fi
 
 echo "OK. Verificación rápida:"
-bridge vlan show
+#bridge vlan show
 ip -4 addr show dev br-lan
 uci show firewall | grep -E "name='(lan10|infra20|iot30|family40|guest50|admin60)'"
+
+
+echo "Bloqueamos uHTTPd dese otras VLAN..."
+uci add firewall rule
+uci set firewall.@rule[-1].name='Allow-LuCI-from-admin60'
+uci set firewall.@rule[-1].src='admin60'
+uci set firewall.@rule[-1].proto='tcp'
+uci set firewall.@rule[-1].dest_port='8080 8443'
+uci set firewall.@rule[-1].target='ACCEPT'
+uci commit firewall
+/etc/init.d/firewall restart
